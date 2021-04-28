309b84fe1748f5449b9ac72e934062a5d9bd59e5
Raahim Naeem <raahim.naeem@yahoo.com>
2021-04-28 16:01:50 +0500

    Update order status celery bg process

--- order/tasks.py
+++ order/tasks.py
@@ -1,12 +1,16 @@
 from celery.schedules import crontab
 from celery.task import periodic_task
+from celery.utils.log import get_task_logger
 from .models import *
+from django.db.models import Q
+from datetime import datetime
+logger = get_task_logger(__name__)
+
 
 @periodic_task(run_every=(crontab(minute='*/1')), name="update_order_status", ignore_result=False)
 def update_order_status():
-    print('here')
-    order_detail = OrderDetail.objects.get(id=1)
-    print(order_detail)
-    order_detail.status = 'rejected'
-    order_detail.save()
+    current_datetime = datetime.now()
+    order_detail = OrderDetail.objects.filter(Q(order_delivery_datetime__lte=current_datetime))
+    order_detail.update(status='rejected')
+    logger.info(order_detail)
     return 1
