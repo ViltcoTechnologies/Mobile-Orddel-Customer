dd143803f287ff22ade6c185050670c9d17a6ffb
amirnawaz786 <amir.nawaz@viltco.com>
2021-11-03 09:47:34 +0500

    git updated

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  orddel
+//
+//  Created by Amir on 10/27/21.
+//
+
+import Foundation
---
+++ ios/orddel/AppDelegate.h
@@ -0,0 +1,12 @@
+#import <React/RCTBridgeDelegate.h>
+#import <UIKit/UIKit.h>
+#import <Firebase/Firebase.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+//@import Firebase;
+
+@end
+
